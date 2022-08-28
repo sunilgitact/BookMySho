79c9a632578738f03cfbd50ff2fe4dc8af932864
Sunil Kumar <110608088+sunilgitact@users.noreply.github.com>
2022-08-28 21:54:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './StyleSheets/slide.css';
+import './StyleSheets/SemiNav.css';
+import './StyleSheets/moviecards.css';
+import './StyleSheets/FeedBack.css';
+import './Api.css';
+import './Play.css';
+import './navbar.css';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
